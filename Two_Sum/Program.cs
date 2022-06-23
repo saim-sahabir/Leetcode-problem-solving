e85dea5edacd4f9238cd782dd76487d2d182ad96
saim-sahabir <saimsahabir12it@gmail.com>
2022-06-23 18:41:08 +0600

    Two Sum in Leetcode

---
+++ Two_Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] TwoSum(int[] nums, int target) {
+    int arrayLength = nums.Length;
+    Dictionary < int, int > resultDictionary = new();
+    //Validations
+    if (nums == null || arrayLength < 2) {
+        return Array.Empty < int > ();
+    }
+    //Logic
+    for (int i = 0; i < arrayLength; i++) {
+        int firstNumber = nums[i];
+        int secondNumber = target - firstNumber;
+        if (resultDictionary.TryGetValue(secondNumber, out int index)) {
+            return new [] {
+                index,
+                i
+            };
+        }
+        //resultDictionary.Add(firstNumber, i);
+        resultDictionary[firstNumber] = i;
+    }
+    return Array.Empty < int > ();;
+}
